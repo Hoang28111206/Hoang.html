745c9feeaf117f1942d41e11dda55daf956e1787
Hoang28111206 <94107961+Hoang28111206@users.noreply.github.com>
2021-11-29 17:47:25 +0700

    first project

---
+++ bt2018/bt2018.js
@@ -0,0 +1,12 @@
+function nhapa() {
+   a = parseInt(prompt('Nhập giá trị A:'))
+}
+
+function nhapb() {
+    b = parseInt(prompt('Nhập giá trị B:'))
+ }
+
+ function tong(a,b) {
+    s = a + b
+    return s
+}
